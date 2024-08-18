194f3cac18c1b2ec1aa5375d4900c2e794179b9f
ange houndjo <houndjoangeplus@gmail.com>
2024-08-18 11:22:55 +0200

    tour of heroes

---
+++ src/app/service/in-memory-data.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Hero } from '../interface/hero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService {
+
+  constructor() { }
+}
